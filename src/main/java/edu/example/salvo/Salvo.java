c1e4dd943addb03d1166911f85f5880b7ea9a728
Michal Kowalewski <mkowalewski83@gmail.com>
2018-10-21 11:35:24 +0200

    added salvo class and repostirory

---
+++ src/main/java/edu/example/salvo/Salvo.java
@@ -0,0 +1,48 @@
+package edu.example.salvo;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Salvo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private long turnNo;
+
+    @ElementCollection
+    @Column(name="location")
+    private List<String> locations = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "gamePlayerId")
+    private GamePlayer gamePlayer;
+
+    public Salvo(){}
+
+    public Salvo(long turnNo){
+        this.turnNo = turnNo;
+    }
+
+    public void setLocations(List<String> locations) {
+        this.locations = locations;
+    }
+
+    public void setGamePlayer(GamePlayer gamePlayer) {
+        this.gamePlayer = gamePlayer;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getTurnNo() {
+        return turnNo;
+    }
+
+    public List<String> getLocations() {
+        return locations;
+    }
+}
--- src/main/java/edu/example/salvo/SalvoRepository.java
+++ src/main/java/edu/example/salvo/SalvoRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ShipRepository extends JpaRepository<Ship, Long> {
-    Ship findById(long id);
+public interface SalvoRepository extends JpaRepository<Salvo, Long> {
+    Salvo findById(long id);
 }
